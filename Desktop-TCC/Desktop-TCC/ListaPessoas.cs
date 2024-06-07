9ebae9e7944b0c48dc631b4234f3a3c575e1fc3a
Rafael Jannini <rsjannini@gmail.com>
2024-06-07 09:14:40 -0300

    Criação ListaPessoas form

--- Desktop-TCC/Desktop-TCC/ListaPessoas.cs
+++ Desktop-TCC/Desktop-TCC/ListaPessoas.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Desktop_TCC
 {
-    public partial class Form1 : Form
+    public partial class ListaPessoas : Form
     {
-        public Form1()
+        public ListaPessoas()
         {
             InitializeComponent();
         }
